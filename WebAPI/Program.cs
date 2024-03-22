f53b99aed29291d1f45a197ea1aa28705bb136f9
Onur Kerem Bozkurt <Onur Kerem Bozkurt@DESKTOP-ITNUH9F>
2024-03-22 15:39:06 +0100

    First homework of the week 12 completed

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,7 +1,11 @@
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 using Business.Abstract;
 using Business.Concrete;
+using Business.DependencyResolvers.Autofac;
 using DataAccess.Abstract;
 using DataAccess.Concrete.EntityFramework;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -12,23 +16,30 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
-builder.Services.AddSingleton<ICustomerService,CustomerManager>();
-builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
 
-builder.Services.AddSingleton<IBrandService,BrandManager>();
-builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    // AutofacBusinessModule'u ekleyerek dependency'leri çözme
+    builder.RegisterModule(new AutofacBusinessModule());
+});
+//builder.Services.AddSingleton<ICustomerService,CustomerManager>();
+//builder.Services.AddSingleton<ICustomerDal,EfCustomerDal>();
 
-builder.Services.AddSingleton<IRentalService,RentalManager>();
-builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
+//builder.Services.AddSingleton<IBrandService,BrandManager>();
+//builder.Services.AddSingleton<IBrandDal,EfBrandDal>();
 
-builder.Services.AddSingleton<IUserService,UserManager>();
-builder.Services.AddSingleton<IUserDal, EfUserDal>();
+//builder.Services.AddSingleton<IRentalService,RentalManager>();
+//builder.Services.AddSingleton<IRentalDal, EfRentalDal>();
 
-builder.Services.AddSingleton<IColorService,ColorManager>();
-builder.Services.AddSingleton<IColorDal,EfColorDal>();
+//builder.Services.AddSingleton<IUserService,UserManager>();
+//builder.Services.AddSingleton<IUserDal, EfUserDal>();
 
-builder.Services.AddSingleton<ICarService,CarManager>();
-builder.Services.AddSingleton<ICarDal,EfCarDal>();
+//builder.Services.AddSingleton<IColorService,ColorManager>();
+//builder.Services.AddSingleton<IColorDal,EfColorDal>();
+
+//builder.Services.AddSingleton<ICarService,CarManager>();
+//builder.Services.AddSingleton<ICarDal,EfCarDal>();
 
 
 var app = builder.Build();
